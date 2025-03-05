1607b3335668303fdc2dd45beb14b114b2ae8d98
Sam23656 <sem26m06@gmail.com>
2025-03-05 22:41:11 +0500

    Update Header 'Add styles'

--- spbu_site/src/components/main/Header.tsx
+++ spbu_site/src/components/main/Header.tsx
@@ -1,19 +1,72 @@
-import './Header.scss'
+import './Header.scss';
+import { useState } from 'react';
+import { ChevronDown } from 'lucide-react';
 
-
-function Header(){
-    return (
-        <div className='header'>
-            <div className='header-item'><a href="university">Университет</a></div>
-            <div className='header-item'><a href="applicants">Поступающим</a></div>
-            <div className='header-item'><a href="education">Образование</a></div>
-            <div className='header-item'><a href="students">Студентам</a></div>
-            <div className='header-item'><a href="TRKI">ТРКИ</a></div>
-            <div className='header-item'><a href="documents">Документы</a></div>
-        </div>
-
-)
-    
+interface MenuItems {
+  [key: string]: string[];
 }
 
-export default Header
+const menuItems: MenuItems = {
+  "Университет": ['Об университете', 'Филиал в Ташкенте'],
+  "Образование": ['Бакалавриат', 'Магистратура'],
+  "Поступающим": ['Приём', 'Личный кабинет поступающего'],
+  "Студентам": ['Расписание занятий', 'Научная библиотека имени М. Горького', 'Личный кабинет студента'],
+  "ТРКИ": ['Общая информация', 'Подготовка к экзамену'],
+  "Новости": ['Основные новости', 'Мероприятия', 'Объявления', 'Галерея'],
+  "Документы": ['Основные документы', 'Соглашения', 'Локальные документы'],
+  "Контакты": [],
+};
+
+const menuLinks: MenuItems = {
+  "Университет": ['/about/', '/branch-tashkent/'],
+  "Образование": ['/undergraduate/', '/graduate/'],
+  "Поступающим": ['/admissions/', '/applicant-account/'],
+  "Студентам": ['/class-schedule/', '/library/', '/student-account/'],
+  "ТРКИ": ['/general-info/', '/exam-preparation/'],
+  "Новости": ['/news/', '/events/', '/announcements/', '/gallery/'],
+  "Документы": ['/main-documents/', '/agreements/', '/local-documents/'],
+  "Контакты": [],
+};
+
+function Header() {
+  const [openMenu, setOpenMenu] = useState<string | null>(null);
+
+  const toggleMenu = (menu: string) => {
+    if (menuItems[menu].length === 0) {
+      setOpenMenu(null);
+    } else {
+      setOpenMenu(openMenu === menu ? null : menu);
+    }
+  };
+
+  return (
+    <div className="header">
+      {Object.keys(menuItems).map((key) => (
+        <div key={key} className="relative">
+          <button
+            className="header-item flex items-center space-x-1 text-sm"
+            onMouseEnter={() => toggleMenu(key)}
+          >
+            <span>{key}</span>
+            {menuItems[key].length > 0 && <ChevronDown size={16} />}
+          </button>
+          {openMenu === key && menuItems[key].length > 0 && (
+            <div className="dropdown-menu">
+              {menuItems[key].map((item, index) => (
+                <a
+                  key={item}
+                  href={menuLinks[key][index]}
+                  className="dropdown-item text-sm"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Header;
