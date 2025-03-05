e2b95d66fd84fb78ce854f697d3ca473703d9194
Sam <78599455+Sam23656@users.noreply.github.com>
2025-03-05 21:58:20 +0500

    Update StartPage.tsx

--- spbu_site/src/components/main/StartPage.tsx
+++ spbu_site/src/components/main/StartPage.tsx
@@ -4,8 +4,6 @@ import './StartPage.scss'
 function StartPage() {
     return (
         <div className="">
-            <div className='image'><img src="logo.png" alt="Filial Logo"/></div>
-            
         </div>
     );
 }
