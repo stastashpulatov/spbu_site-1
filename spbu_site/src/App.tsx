ec703e36e7089aa519c9c8e232145f04af2e8584
stastashpulatov <tshstanislav@gmail.com>
2025-04-04 22:17:54 +0500

    Something new

--- spbu_site/src/App.tsx
+++ spbu_site/src/App.tsx
@@ -18,7 +18,8 @@ import Admission from "./components/pages/Admission";
 import TRKIGeneralInfo from "./components/pages/TRKIGeneralInfo";
 import ExamPreparation from "./components/pages/ExamPreparation";
 import Contacts from "./components/pages/Contacts";
-
+import Library from "./components/pages/Library";
+import StudentAccount from "./components/pages/StudentAccount";
 import Documents from "./components/pages/Documents";
 import Agreements from "./components/pages/Agreements";
 
@@ -55,6 +56,8 @@ function App() {
                 <Route path='/contacts' element={<Contacts />} />
                 <Route path='/documents' element={<Documents />} />
                 <Route path='/agreements' element={<Agreements />} />
+                <Route path='/library' element={<Library />} />
+                <Route path='/student-account' element={<StudentAccount />} />
 
                 {/* Admin Routes */}
                 <Route path='/admin' element={<Navigate to="/admin/login" replace />} />
@@ -69,39 +72,6 @@ function App() {
                 />
               </Routes>
             </TransitionWrapper>
-            <Routes>
-              <Route path='/' element={<TransitionWrapper><StartPage /></TransitionWrapper>} />
-              <Route path='/about' element={<TransitionWrapper><About /></TransitionWrapper>} />
-              <Route path='/about/history' element={<TransitionWrapper><History /></TransitionWrapper>} />
-              <Route path='/about/info' element={<TransitionWrapper><UniversityInfo /></TransitionWrapper>} />
-              <Route path='/branch-tashkent/about_filial' element={<TransitionWrapper><BranchAbout /></TransitionWrapper>} />
-              <Route path='/branch-tashkent/leadership' element={<TransitionWrapper><Leadership /></TransitionWrapper>} />
-              <Route path='/education/bachelor/economy' element={<TransitionWrapper><InternationalBusiness /></TransitionWrapper>} />
-              <Route path='/education/bachelor/programming' element={<TransitionWrapper><Programming /></TransitionWrapper>} />
-              <Route path='/education/master/digital-entrepreneurship' element={<TransitionWrapper><DigitalEntrepreneurship /></TransitionWrapper>} />
-              <Route path='/education/master/international-private-law' element={<TransitionWrapper><InternationalPrivateLaw /></TransitionWrapper>} />
-              <Route path='/education/master/modern-china' element={<TransitionWrapper><ModernChina /></TransitionWrapper>} />
-              <Route path='/admission' element={<TransitionWrapper><Admission /></TransitionWrapper>} />
-              <Route path='/trki/general-info' element={<TransitionWrapper><TRKIGeneralInfo /></TransitionWrapper>} />
-              <Route path='/trki/exam-preparation' element={<TransitionWrapper><ExamPreparation /></TransitionWrapper>} />
-              <Route path='/contacts' element={<TransitionWrapper><Contacts /></TransitionWrapper>} />
-
-              <Route path='/documents' element={<TransitionWrapper><Documents /></TransitionWrapper>} />
-              <Route path='/agreements' element={<TransitionWrapper><Agreements /></TransitionWrapper>} />
-
-
-              {/* Admin Routes */}
-              <Route path='/admin' element={<Navigate to="/admin/login" replace />} />
-              <Route path='/admin/login' element={<AdminLogin />} />
-              <Route
-                path='/admin/dashboard'
-                element={
-                  <ProtectedRoute>
-                    <AdminDashboard />
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
           </Router>
         </div>
       </LanguageProvider>
