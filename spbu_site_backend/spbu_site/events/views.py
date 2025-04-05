9d489cd06e7001baafb2c8923f1f97eaca4f428a
Sam23656 <sem26m06@gmail.com>
2025-04-05 16:09:10 +0500

    Add Permissions To Events

--- spbu_site_backend/spbu_site/events/views.py
+++ spbu_site_backend/spbu_site/events/views.py
@@ -1,32 +1,46 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from .permissions import IsAdminOrTeacher
 
 from .models import News, Event
 from .serializers import NewsSerializer, EventSerializer
 
 
+
 # Create your views here.
 
 class NewsViewSet(viewsets.ModelViewSet):
-    queryset = News.objects.filter(is_visible=True).order_by('-publication_date')
     serializer_class = NewsSerializer
     pagination_class = PageNumberPagination
+    permission_classes = [IsAdminOrTeacher]
+
+    def get_queryset(self):
+        queryset = News.objects.all().order_by('-publication_date')
+        if not (self.request.user.is_staff or getattr(self.request.user, 'role', None) in ['admin', 'teacher']):
+            queryset = queryset.filter(is_visible=True)
+        return queryset
 
     def get(self, request):
         paginator = self.pagination_class()
-        result_page = paginator.paginate_queryset(self.queryset, request)
+        result_page = paginator.paginate_queryset(self.get_queryset(), request)
         serializer = self.serializer_class(result_page, many=True, context={'request': request})
         return paginator.get_paginated_response(serializer.data)
 
 
 class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.filter(is_visible=True).order_by('-date')
     serializer_class = EventSerializer
     pagination_class = PageNumberPagination
+    permission_classes = [IsAdminOrTeacher]
+
+    def get_queryset(self):
+        queryset = Event.objects.all().order_by('-date')
+        if not (self.request.user.is_staff or getattr(self.request.user, 'role', None) in ['admin', 'teacher']):
+            queryset = queryset.filter(is_visible=True)
+        return queryset
 
     def get(self, request):
         paginator = self.pagination_class()
-        result_page = paginator.paginate_queryset(self.queryset, request)
+        result_page = paginator.paginate_queryset(self.get_queryset(), request)
         serializer = self.serializer_class(result_page, many=True, context={'request': request})
         return paginator.get_paginated_response(serializer.data)
--- spbu_site_backend/spbu_site/spbu_site/urls.py
+++ spbu_site_backend/spbu_site/spbu_site/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/user/', include('users.urls')),
     path('admin/', admin.site.urls),
+    path('api/user/', include('users.urls')),
+    path('api/events/', include('events.urls')),
 ]
