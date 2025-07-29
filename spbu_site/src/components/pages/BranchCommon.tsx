571ca56f0ee7d2712e1fa96eb3c4d4791a9adf6c
Sam <sem26m06@gmail.com>
2025-07-29 23:41:29 +0500

    Update BranchCommon

--- spbu_site/src/components/pages/BranchCommon.tsx
+++ spbu_site/src/components/pages/BranchCommon.tsx
@@ -16,6 +16,23 @@ type Translations = {
       field: string;
       value: React.ReactNode;
     }[];
+    founders: {
+      title: string;
+      tableHeaders: {
+        name: string;
+        address: string;
+        phones: string;
+        email: string;
+        website: string;
+      };
+      tableData: {
+        name: string;
+        address: string;
+        phones: string;
+        email: React.ReactNode;
+        website: React.ReactNode;
+      }[];
+    };
   };
 };
 
@@ -29,139 +46,215 @@ const translations: Translations = {
     tableData: [
       {
         field: 'Полное наименование образовательной организации',
-        value: ''
+        value: 'Федеральное государственное бюджетное образовательное учреждение высшего образования «Санкт-Петербургский государственный университет»'
       },
       {
         field: 'Сокращенное наименование образовательной организации (при наличии)',
-        value: ''
+        value: 'Санкт-Петербургский государственный университет, Санкт-Петербургский университет, СПбГУ'
       },
       {
         field: 'Дата создания образовательной организации',
-        value: ''
+        value: 'Санкт-Петербургский государственный университет учрежден указом Петра I от 22 января 1724 года, введенным в действие указом Правительствующего Сената от 28 января 1724 года.'
+      },
+      {
+        field: 'Учредитель образовательной организации',
+        value: <p>Учредителем и собственником имущества Санкт-Петербургского государственного университета является Российская Федерация. Функции и полномочия учредителя от имени Российской Федерации выполняет <a href='http://government.ru/'>Правительство Российской Федерации.</a></p>
       },
       {
         field: 'Адрес местонахождения образовательной организации',
-        value: ''
+        value: 'Россия, 199034, Санкт-Петербург, Университетская наб., д. 7–9'
       },
       {
         field: 'Филиалы образовательной организации',
-        value: ''
+        value: 'Имеются'
       },
       {
         field: 'Представительства образовательной организации',
-        value: ''
+        value: 'Имеются'
       },
       {
         field: 'Режим, график работы',
-        value: <>
-          
-        </>
+        value: <ul>
+          <li>Пн-пт — 8:00–22:00</li>
+          <li>Сб — 8:00–18:00</li>
+          <li>Вс — выходной</li>
+          <li><a href='http://government.ru/'>Подробнее о трудовом распорядке СПбГУ</a></li>
+         </ul>
       },
       {
         field: 'Контактные телефоны',
-        value: ''
+        value: '+7 (812) 325–87–36 (факс)'
       },
       {
         field: 'Адреса электронной почты',
-        value: ''
+        value: 'spbu@spbu.ru'
       }
-    ]
+    ],
+    founders: {
+      title: 'Учредители образовательной организации:',
+      tableHeaders: {
+        name: 'Наименование учредителя',
+        address: 'Адрес местонахождения учредителя(ей)',
+        phones: 'Контактные телефоны',
+        email: 'Адрес электронной почты',
+        website: 'Адрес сайта учредителя(ей) в сети «Интернет»'
+      },
+      tableData: [
+        {
+          name: 'Правительство Российской Федерации',
+          address: 'Россия, 103274, Москва, Краснопресненская наб., д. 2',
+          phones: '+7-800-200-84-42',
+          email: <a href='http://services.government.ru/letters/'>http://services.government.ru/letters/</a>,
+          website: <a href="http://government.ru">http://government.ru</a>
+        }
+      ]
+    }
   },
   uz: {
     title: 'Asosiy ma\'lumotlar',
     tableHeaders: {
-      field: 'Nomi',
+      field: 'Maydon nomi',
       value: 'Qiymati'
     },
     tableData: [
       {
-        field: 'Ta\'lim tashkilotining to\'liq nomi',
-        value: 'O\'zbekiston Respublikasi Olmaliq shahridagi "MISIS" Milliy tadqiqot texnologiya universiteti federal davlat avtonom oliy ta\'lim muassasasi filiali, "MISIS" MTTU Olmaliq filiali "MISIS" MTTU OF'
+        field: 'Ta\'lim muassasasining to\'liq nomi',
+        value: '«Sankt-Peterburg davlat universiteti» federal davlat byudjet oliy ta\'lim muassasasi'
       },
       {
-        field: 'Ta\'lim tashkilotining qisqartirilgan nomi (mavjud bo\'lsa)',
-        value: '"MISIS" MTTU Olmaliq filiali "MISIS" MTTU OF'
+        field: 'Ta\'lim muassasasining qisqartirilgan nomi (agar mavjud bo\'lsa)',
+        value: 'Sankt-Peterburg davlat universiteti, Sankt-Peterburg universiteti, SPbGU'
       },
       {
-        field: 'Ta\'lim tashkiloti tashkil etilgan sana',
-        value: '11.05.2018'
+        field: 'Ta\'lim muassasasining tashkil etilgan sanasi',
+        value: 'Sankt-Peterburg davlat universiteti Pyotr I ning 1724 yil 22 yanvardagi farmoni bilan tashkil etilgan bo\'lib, 1724 yil 28 yanvardagi Senat farmoni bilan amalga oshirilgan.'
       },
       {
-        field: 'Ta\'lim tashkilotining joylashgan manzili',
-        value: 'O\'zbekiston Respublikasi, Toshkent viloyati, Olmaliq shahri, Amir Temur ko\'chasi, 56-uy'
+        field: 'Ta\'lim muassasasining asoschisi',
+        value: <p>Sankt-Peterburg davlat universitetining asoschisi va mulk egasi Rossiya Federatsiyasidir. Asoschining vakolatlarini Rossiya Federatsiyasi nomidan <a href='http://government.ru/'>Rossiya Federatsiyasi Hukumati</a> amalga oshiradi.</p>
       },
       {
-        field: 'Ta\'lim tashkilotining filiallari',
-        value: 'mavjud emas'
+        field: 'Ta\'lim muassasasining joylashgan manzili',
+        value: 'Rossiya, 199034, Sankt-Peterburg, Universitetskaya nab., 7–9'
       },
       {
-        field: 'Ta\'lim tashkilotining vakolatxonalari',
-        value: 'mavjud emas'
+        field: 'Ta\'lim muassasasining filiallari',
+        value: 'Mavjud'
+      },
+      {
+        field: 'Ta\'lim muassasasining vakolatxonalari',
+        value: 'Mavjud'
       },
       {
         field: 'Ish tartibi, ish jadvali',
-        value: <>
-          Dushanba - Juma: 9:00 - 18:00. Tushlik: 13:00 - 14:00.<br/>
-          Shanba: 9:00 - 16:00
-        </>
+        value: <ul>
+          <li>Dushanba-Juma: 8:00–22:00</li>
+          <li>Shanba: 8:00–18:00</li>
+          <li>Yakshanba: dam olish kuni</li>
+          <li><a href='http://government.ru/'>SPbGU mehnat tartibi haqida batafsil</a></li>
+         </ul>
       },
       {
         field: 'Aloqa telefonlari',
-        value: '+998 70 614-22-64:'
+        value: '+7 (812) 325–87–36 (faks)'
       },
       {
         field: 'Elektron pochta manzillari',
-        value: 'info@misis.uz'
+        value: 'spbu@spbu.ru'
       }
-    ]
+    ],
+    founders: {
+      title: 'Ta\'lim muassasasining asoschilari:',
+      tableHeaders: {
+        name: 'Asoschining nomi',
+        address: 'Asoschining joylashgan manzili',
+        phones: 'Aloqa telefonlari',
+        email: 'Elektron pochta manzili',
+        website: '«Internet» tarmog\'idagi asoschining veb-sayt manzili'
+      },
+      tableData: [
+        {
+          name: 'Rossiya Federatsiyasi Hukumati',
+          address: 'Rossiya, 103274, Moskva, Krasnopresnenskaya nab., 2-uy',
+          phones: '+7-800-200-84-42',
+          email: <a href='http://services.government.ru/letters/'>http://services.government.ru/letters/</a>,
+          website: <a href="http://government.ru">http://government.ru</a>
+        }
+      ]
+    }
   },
   en: {
-    title: 'Basic information',
+    title: 'Basic Information',
     tableHeaders: {
-      field: 'Name',
+      field: 'Field Name',
       value: 'Value'
     },
     tableData: [
       {
         field: 'Full name of the educational organization',
-        value: 'Branch of the federal state autonomous educational institution of higher education "National Research Technological University "MISIS" in Almalyk, Republic of Uzbekistan, Almalyk branch of NUST "MISIS" AB NUST "MISIS"'
+        value: 'Federal State Budgetary Educational Institution of Higher Education "Saint Petersburg State University"'
       },
       {
-        field: 'Abbreviated name of the educational organization (if any)',
-        value: 'Almalyk branch of NUST "MISIS" AB NUST "MISIS"'
+        field: 'Abbreviated name of the educational organization (if available)',
+        value: 'Saint Petersburg State University, Saint Petersburg University, SPbU'
       },
       {
         field: 'Date of establishment of the educational organization',
-        value: '11.05.2018'
+        value: 'Saint Petersburg State University was established by the decree of Peter I on January 22, 1724, enacted by the decree of the Governing Senate on January 28, 1724.'
       },
       {
-        field: 'Address of the educational organization',
-        value: 'Republic of Uzbekistan, Tashkent region, Almalyk, 56 Amir Temur Street'
+        field: 'Founder of the educational organization',
+        value: <p>The founder and owner of the property of Saint Petersburg State University is the Russian Federation. The functions and powers of the founder on behalf of the Russian Federation are performed by the <a href='http://government.ru/'>Government of the Russian Federation.</a></p>
+      },
+      {
+        field: 'Location address of the educational organization',
+        value: 'Russia, 199034, Saint Petersburg, Universitetskaya nab., 7–9'
       },
       {
         field: 'Branches of the educational organization',
-        value: 'none'
+        value: 'Available'
       },
       {
         field: 'Representative offices of the educational organization',
-        value: 'none'
+        value: 'Available'
       },
       {
         field: 'Working hours, schedule',
-        value: <>
-          Monday - Friday: 9:00 AM - 6:00 PM. Lunch: 1:00 PM - 2:00 PM.<br/>
-          Saturday: 9:00 AM - 4:00 PM
-        </>
+        value: <ul>
+          <li>Mon-Fri: 8:00 AM–10:00 PM</li>
+          <li>Sat: 8:00 AM–6:00 PM</li>
+          <li>Sun: day off</li>
+          <li><a href='http://government.ru/'>More about SPbU work regulations</a></li>
+         </ul>
       },
       {
         field: 'Contact phones',
-        value: '+998 70 614-22-64:'
+        value: '+7 (812) 325–87–36 (fax)'
       },
       {
         field: 'Email addresses',
-        value: 'info@misis.uz'
+        value: 'spbu@spbu.ru'
       }
-    ]
+    ],
+    founders: {
+      title: 'Founders of the educational organization:',
+      tableHeaders: {
+        name: 'Founder Name',
+        address: 'Founder Location Address',
+        phones: 'Contact Phones',
+        email: 'Email Address',
+        website: 'Founder Website in the Internet'
+      },
+      tableData: [
+        {
+          name: 'Government of the Russian Federation',
+          address: 'Russia, 103274, Moscow, Krasnopresnenskaya nab., 2',
+          phones: '+7-800-200-84-42',
+          email: <a href='http://services.government.ru/letters/'>http://services.government.ru/letters/</a>,
+          website: <a href="http://government.ru">http://government.ru</a>
+        }
+      ]
+    }
   }
 };
 
@@ -203,27 +296,27 @@ const BranchCommon: React.FC = () => {
           </table>
         </div>
         <div className="founders-section">
-          <h2>Учредители образовательной организации:</h2>
+          <h2>{t.founders.title}</h2>
           <table className="founders-table">
             <thead>
               <tr>
-                <th>Наименование учредителя</th>
-                <th>Фамилия, имя, отчество учредителя (ей) образовательной организации</th>
-                <th>Адрес местонахождения учредителя(ей)</th>
-                <th>Контактные телефоны</th>
-                <th>Адрес электронной почты</th>
-                <th>Адрес сайта учредителя(ей) в сети «Интернет»</th>
+                <th>{t.founders.tableHeaders.name}</th>
+                <th>{t.founders.tableHeaders.address}</th>
+                <th>{t.founders.tableHeaders.phones}</th>
+                <th>{t.founders.tableHeaders.email}</th>
+                <th>{t.founders.tableHeaders.website}</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
+              {t.founders.tableData.map((founder, index) => (
+                <tr key={index}>
+                  <td>{founder.name}</td>
+                  <td>{founder.address}</td>
+                  <td>{founder.phones}</td>
+                  <td>{founder.email}</td>
+                  <td>{founder.website}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
